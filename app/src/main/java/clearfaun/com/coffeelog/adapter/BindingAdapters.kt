f972675342e7012997df30b59ee4d2c5f47e74b4
Spencer Depas <spencerdepas@gmail.com>
2020-01-11 18:27:13 -0500

    work on views

--- app/src/main/java/clearfaun/com/coffeelog/adapter/BindingAdapters.kt
+++ app/src/main/java/clearfaun/com/coffeelog/adapter/BindingAdapters.kt
@@ -1,10 +1,13 @@
 package clearfaun.com.coffeelog.adapter
 
+import android.os.Build
+import android.widget.ImageView
 import androidx.annotation.NonNull
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import clearfaun.com.coffeelog.model.Character
+import com.bumptech.glide.Glide
 
 
 @BindingAdapter("characters")
@@ -22,8 +25,21 @@ fun bindLayoutManager(@NonNull recyclerView: RecyclerView, vertical: Boolean) {
     val orientation = if (vertical) RecyclerView.VERTICAL else RecyclerView.HORIZONTAL
     recyclerView.layoutManager = GridLayoutManager(
         recyclerView.context,
-        1,
+        2,
         orientation,
         false
     )
 }
+
+@BindingAdapter("android:src")
+fun loadImage(imageView: ImageView, url: String) {
+    Glide.with(imageView.context).load(url).into(imageView);
+}
+
+@BindingAdapter("setClipToOutline")
+fun loadImage(imageView: ImageView, clipToOutline: Boolean) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        imageView.setClipToOutline(clipToOutline)
+    }
+}
+
